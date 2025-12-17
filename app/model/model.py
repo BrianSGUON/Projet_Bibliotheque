11e1fce3d5adfeaf006adfcc24d9c53561bd1ddb
BrianSGUON <165809873+BrianSGUON@users.noreply.github.com>
2025-12-17 14:39:55 +0100

    Add files via upload

---
+++ app/model/model.py
@@ -0,0 +1,60 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Enum, Text, UniqueConstraint
+from sqlalchemy.orm import relationship
+import enum
+from .database import Base
+
+class CategoryEnum(enum.Enum):
+    Fiction = "Fiction"
+    Science = "Science"
+    Histoire = "Histoire"
+    Philosophie = "Philosophie"
+    Autre = "Autre"
+
+class LoanStatus(enum.Enum):
+    ACTIF = "actif"
+    RETOURNE = "retourné"
+    RETARD = "en retard"
+
+class Author(Base):
+    __tablename__ = "authors"
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    birth_date = Column(Date, nullable=False)
+    nationality = Column(String(3), nullable=False)
+    biography = Column(Text, nullable=True)
+    death_date = Column(Date, nullable=True)
+    website = Column(String, nullable=True)
+
+    __table_args__ = (UniqueConstraint('firstname', 'lastname', name='_author_full_name_uc'),)
+    books = relationship("Book", back_populates="author")
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    isbn = Column(String(13), unique=True, index=True, nullable=False)
+    publication_year = Column(Integer, nullable=False)
+    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
+    available_copies = Column(Integer, default=1)
+    total_copies = Column(Integer, nullable=False)
+    category = Column(Enum(CategoryEnum), nullable=False)
+    language = Column(String, nullable=False)
+    pages = Column(Integer, nullable=False)
+    publisher = Column(String, nullable=False)
+
+    author = relationship("Author", back_populates="books")
+    loans = relationship("Loan", back_populates="book")
+
+class Loan(Base):
+    __tablename__ = "loans"
+    id = Column(Integer, primary_key=True, index=True)
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+    borrower_name = Column(String, nullable=False)
+    borrower_email = Column(String, nullable=False)
+    loan_date = Column(DateTime, nullable=False)
+    return_deadline = Column(DateTime, nullable=False)
+    actual_return_date = Column(DateTime, nullable=True)
+    status = Column(Enum(LoanStatus), default=LoanStatus.ACTIF)
+
+    book = relationship("Book", back_populates="loans")
